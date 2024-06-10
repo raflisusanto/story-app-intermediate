b6370cf5d277a02e447d8b69211b740a6b0b69e8
Muhamad Rafli Susanto <boyverybolot@gmail.com>
2024-06-10 10:06:03 +0700

    feat: added english localization

--- app/src/main/java/com/example/storyappsubmission/helper/ExtensionFunctions.kt
+++ app/src/main/java/com/example/storyappsubmission/helper/ExtensionFunctions.kt
@@ -2,6 +2,7 @@ package com.example.storyappsubmission.helper
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.graphics.Color
 import android.text.Spannable
 import android.text.SpannableString
@@ -17,6 +18,8 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import java.util.Locale
+
 
 fun AppCompatActivity.setupCombinedText(tvCombinedText: TextView, textResource: Int, targetActivity: Class<*>) {
     val combinedText = getString(textResource)
@@ -29,7 +32,15 @@ fun AppCompatActivity.setupCombinedText(tvCombinedText: TextView, textResource: 
     }
 
     // Set clickable part
-    val clickablePartStart = 27
+    val config: Configuration = resources.configuration
+    val currentLocale: Locale = config.locales.get(0)
+    var clickablePartStart = 27
+
+    if (currentLocale.language.equals(Locale("en").language)) {
+        // Do something specific for English locale
+        clickablePartStart = 25
+    }
+
     spannableString.setSpan(clickableSpan, clickablePartStart, combinedText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
 
     // Set color for clickable part
